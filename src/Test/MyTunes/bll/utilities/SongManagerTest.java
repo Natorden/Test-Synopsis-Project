329348c030baf8a074aa8a2140a73732ed5d4b06
Rolf Geschke <71587437+Natorden@users.noreply.github.com>
2023-12-11 12:35:46 +0100

    small test method

--- src/Test/MyTunes/bll/utilities/SongManagerTest.java
+++ src/Test/MyTunes/bll/utilities/SongManagerTest.java
@@ -1,12 +1,17 @@
 package MyTunes.bll.utilities;
 
+import MyTunes.be.Song;
 import MyTunes.dal.file.SongFileDAO;
 import MyTunes.dal.interfaces.ISongDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SongManagerTest {
 
@@ -25,6 +30,10 @@ class SongManagerTest {
 
     @Test
     void getAllSongs() {
+        List<Song> allSongs = Arrays.asList();
+        when(_SongManagerMock.getAllSongs()).thenReturn(allSongs);
+
+        //assert here (_SongManagerMock.getAllSongs());
     }
 
     @Test
