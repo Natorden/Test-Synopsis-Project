cc021bcb312a322a738936f1edd589ffb3655723
AdamGalla <ado.galla@gmail.com>
2023-12-12 11:27:33 +0100

    Update PlaylistManagerTest.java

--- src/Test/MyTunes/bll/utilities/PlaylistManagerTest.java
+++ src/Test/MyTunes/bll/utilities/PlaylistManagerTest.java
@@ -1,16 +1,21 @@
 package MyTunes.bll.utilities;
 
 import MyTunes.be.Playlist;
-
+import MyTunes.be.PlaylistRelation;
+import MyTunes.be.Song;
 import MyTunes.dal.file.PlaylistFileDAO;
 import MyTunes.dal.file.PlaylistRelationsFileDAO;
 import MyTunes.dal.interfaces.IPlaylistDAO;
 import MyTunes.dal.interfaces.IPlaylistRelationsDAO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -36,6 +41,7 @@ class PlaylistManagerTest {
         allPlaylists.add(new Playlist(1, "Playlist 1"));
         allPlaylists.add(new Playlist(2, "Playlist 2"));
 
+        when(_PlaylistMock.getAllPlaylists()).thenReturn(allPlaylists);
         when(_PlaylistMock.getAllPlaylists()).thenReturn(allPlaylists);
     }
 
@@ -64,15 +70,54 @@ class PlaylistManagerTest {
     void addSongToPlaylist() {
     }
 
-    @Test
-    void removeSongFromPlaylist() {
+    static Stream<Arguments> songProvider() {
+        return Stream.of(
+                Arguments.of(new Playlist(1, "Playlist 1"),
+                        new Song(1, "Song 1", "C:\\Song1.mp3", "00:00:00", "Artist 1", "Category 1", "Album 1"),
+                        new Song(2, "Song 2", "C:\\Song2.mp3", "00:00:00", "Artist 2", "Category 2", "Album 2")));
     }
 
-    @Test
-    void moveSongUpInPlaylist() {
+    @ParameterizedTest
+    @MethodSource("songProvider")
+    void removeSongFromPlaylist(Playlist playlist, Song song1, Song song2) {
+        // Arrange
+        playlist.addSong(new PlaylistRelation(playlist, song1.getId()));
+        playlist.addSong(new PlaylistRelation(playlist, song2.getId()));
+        // Act
+        playlist.removeSong(song1);
+        // Assert
+        assertEquals(1, playlist.getSongs().size());
     }
 
-    @Test
-    void moveSongDownInPlaylist() {
+    @ParameterizedTest
+    @MethodSource("songProvider")
+    void moveSongUpInPlaylist(Playlist playlist, Song song1, Song song2) {
+        // Arrange
+        PlaylistRelation relation1 = new PlaylistRelation(playlist, song1.getId(), 1);
+        PlaylistRelation relation2 = new PlaylistRelation(playlist, song2.getId(), 2);
+        relation1.setSong(song1);
+        relation2.setSong(song2);
+        playlist.addSong(relation1);
+        playlist.addSong(relation2);
+        // Act
+        _PlaylistManager.moveSongUpInPlaylist(playlist, song2);
+        // Assert
+        assertEquals(2, playlist.getRelations().getFirst().getOrderId());
+    }
+
+    @ParameterizedTest
+    @MethodSource("songProvider")
+    void moveSongDownInPlaylist(Playlist playlist, Song song1, Song song2) {
+        // Arrange
+        PlaylistRelation relation1 = new PlaylistRelation(playlist, song1.getId(), 1);
+        PlaylistRelation relation2 = new PlaylistRelation(playlist, song2.getId(), 2);
+        relation1.setSong(song1);
+        relation2.setSong(song2);
+        playlist.addSong(relation1);
+        playlist.addSong(relation2);
+        // Act
+        _PlaylistManager.moveSongDownInPlaylist(playlist, song1);
+        // Assert
+        assertEquals(2, playlist.getRelations().getFirst().getOrderId());
     }
 }
